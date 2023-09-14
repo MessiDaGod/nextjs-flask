1a92a67ad27cf96d30b69161be2c69f3337581a5
joeshakely <joeshakely@gmail.com>
2023-09-14 14:00:08 -0700

    hell ya!

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -2,6 +2,11 @@
 import React, { useState } from "react";
 import mammoth from "mammoth";
 import { useSearchParams } from "next/navigation";
+import * as pdfjs from 'pdfjs-dist/build/pdf';
+
+// This path might need to be adjusted based on where your Next.js public folder and node_modules are.
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.js';
+
 
 export default function WordToMd() {
   const [file, setFile] = useState<File | null>(null);
@@ -16,6 +21,69 @@ export default function WordToMd() {
       console.log(files[0])
     }
   };
+
+  async function pdfToImages(pdfData: any) {
+    const loadingTask = pdfjs.getDocument({data: pdfData});
+    const pdf = await loadingTask.promise;
+    const numPages = pdf.numPages;
+    const images = [];
+
+    for (let i = 1; i <= numPages; i++) {
+      const page = await pdf.getPage(i);
+      const scale = 1.5; // adjust as needed
+      const viewport = page.getViewport({scale});
+
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      canvas.width = viewport.width;
+      canvas.height = viewport.height;
+
+      //@ts-ignore
+      await page.render({canvasContext: ctx, viewport}).promise;
+
+      images.push(canvas.toDataURL());
+    }
+
+    return images;
+  }
+
+  // Usage:
+  // const images = await pdfToImages(somePdfData);
+
+  async function onSubmitJs(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (!file) {
+      alert("Please select a file before submitting.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const reader = new FileReader();
+    reader.onload = async (event) => {
+      const pdfData = event.target?.result;
+      if (pdfData) {
+        const images = await pdfToImages(pdfData);
+        // Now, you can handle these images as you wish.
+        // Example: displaying them or providing download links as shown in your existing code
+        images.forEach((dataUrl, index) => {
+          const img = document.createElement('img');
+          img.src = dataUrl;
+          document.body.appendChild(img);
+
+          const link = document.createElement('a');
+          link.href = dataUrl;
+          link.download = `image_${index}.png`;
+          link.textContent = `Download Image ${index}`;
+          document.body.appendChild(link);
+        });
+      }
+    };
+    reader.readAsArrayBuffer(file);
+  }
+
 
   const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -35,13 +103,13 @@ export default function WordToMd() {
 
     const data = await response.json();
 
-    // Handle the response data for both PDF and DOCX types
-    if (data.message) {
-      alert(data.message);
-    } else if (data.error) {
-      alert(data.error);
-      return;
-    }
+    // // Handle the response data for both PDF and DOCX types
+    // if (data.message) {
+    //   alert(data.message);
+    // } else if (data.error) {
+    //   alert(data.error);
+    //   return;
+    // }
 
     if (type === "pdf" && data.images) {
       // Display images and provide download links
@@ -56,6 +124,8 @@ export default function WordToMd() {
         link.textContent = `Download Image ${index}`;
         document.body.appendChild(link);
       });
+
+      return; // Make sure to return after handling the PDF to prevent the rest of the function from executing
     } else if (type === "docx") {
       // Convert the DOCX file to HTML using mammoth.js
       const reader = new FileReader();
@@ -76,7 +146,11 @@ export default function WordToMd() {
 
       <form onSubmit={onSubmit}>
         <input type="file" accept=".docx,.pdf" onChange={onFileChange} />
-        <button type="submit">Upload and Convert</button>
+        <button type="submit">Convert Python</button>
+      </form>
+      <form onSubmit={onSubmitJs}>
+        <input type="file" accept=".docx,.pdf" onChange={onFileChange} />
+        <button type="submit">Convert JS</button>
       </form>
 
       {docxHtml && (
--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, options) => {
+    // Exclude .node files from bundling
+    config.externals = config.externals || [];
+    config.externals.push({ canvas: 'commonjs canvas' });
+
+    // Add a rule to load .node files
+    config.module.rules.push({
+      test: /\.node$/,
+      use: 'node-loader',
+    });
+
+    return config;
+  },
   rewrites: async () => {
     return [
       {
@@ -9,8 +22,8 @@ const nextConfig = {
             ? 'http://127.0.0.1:5328/api/:path*'
             : '/api/',
       },
-    ]
+    ];
   },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
