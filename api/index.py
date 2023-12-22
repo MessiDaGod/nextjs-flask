710db94cf62c0422a22859700b3a131cab488865
joeshakely <joeshakely@gmail.com>
2023-12-22 12:28:02 -0800

    okok

--- api/index.py
+++ api/index.py
@@ -13,7 +13,7 @@ logger = logging.getLogger('tdm')
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
-@app.route('/api/python', methods=['POST'])
+@app.route('/api/calculate', methods=['POST'])
 def calculate():
     logger.info('Received request for /calculate')
 
@@ -24,28 +24,28 @@ def calculate():
         logger.info('CSV data loaded successfully')
 
         # Normalize monetary columns by replacing $- with 0 and converting to float
-        monetary_columns = ['Higher End']
+        monetary_columns = ['Salary']
         for col in monetary_columns:
             df[col] = df[col].replace({'\$-': '0', '\$': '', ',': ''}, regex=True).astype(float)
 
-        # Define your weights
-        weights = {
-            'Source': 1 if df['Source'].str.contains('Personal Experience').any() else 0.8,
-            'Experience at Time': 1,
-            'Region': 1 if df['Region'].str.contains('San Diego').any() else 0.8,
-        }
+        # Define weights
+        df['Source Weight'] = df['Source'].apply(lambda x: 1 if 'Personal Experience' in x else 0.8)
+        df['Region Weight'] = df['Region'].apply(lambda x: 1 if 'San Diego' in x else 0.8)
+        df['Experience Weight'] = df['Experience at Time'].apply(lambda x: 1 if x == -1 else 1)
+
+        # Increased skill level factor (example: 10% increase)
+        skill_increase_factor = 1.06
 
         # Apply weights and calculate weighted score
-        df['Weighted Score'] = df['Higher End'] * weights['Source']
-        df['Weighted Score'] = df['Higher End'] * weights['Experience at Time']
-        df['Weighted Score'] = df['Higher End'] * weights['Region']
+        df['Weighted Score'] = df.apply(lambda row: row['Salary'] * row['Source Weight'] * row['Region Weight'] * skill_increase_factor if row['Experience at Time'] == -1 else row['Salary'] * row['Source Weight'] * row['Region Weight'] * row['Experience Weight'] * skill_increase_factor, axis=1)
+
 
         # Calculate the weighted average salary
         weighted_avg_salary = df['Weighted Score'].mean()
         formatted_salary = "${:,.2f}".format(weighted_avg_salary)
 
         logger.info('Calculations completed')
-        return jsonify({"Raw Data": df['Higher End'], "Weighted Average Salary": formatted_salary})
+        return jsonify({"Weighted Average Salary": formatted_salary})
     except Exception as e:
         logger.error('Error occurred during calculation: %s', e)
         return jsonify({"error": str(e)}), 500
--- kill_port.sh
+++ kill_port.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-lsof -n -i4TCP:3000| grep LISTEN | awk '{ print $2 }' | xargs kill -9
+lsof -n -i4TCP:3001| grep LISTEN | awk '{ print $2 }' | xargs kill -9
 lsof -n -i4TCP:5328| grep LISTEN | awk '{ print $2 }' | xargs kill -9
 rm -rf api/__pycache__
